eca8056f1956629fa35b3cb9ccecee79cbb38d66
deffbee <dfbunk@gmail.com>
2017-04-20 09:15:15 -0400

    Errors are within CreateAccountActivity regarding the checks for email validity.

--- app/src/main/java/com/example/pc/shindigz11/CreateAccountActivity.java
+++ app/src/main/java/com/example/pc/shindigz11/CreateAccountActivity.java
@@ -1,15 +1,20 @@
 package com.example.pc.shindigz11;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText password, email, confirmPass;
     Button login;
     String emailText, passwordText, confirmPassText;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +30,27 @@ public class CreateAccountActivity extends AppCompatActivity {
         passwordText = password.getText().toString();
         confirmPassText = confirmPass.getText().toString();
 
+        public final boolean isValidEmail(String emailText) {
+
+       if (emailText != null && android.util.Patterns.EMAIL_ADDRESS.matcher(emailText).matches())
+       {
+           return true;
+       }
+       else
+       {
+         email   (getResources().getString(R.string.email_error));
+       }
 
 
 
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (email, password, )
+        }
     }
 }
